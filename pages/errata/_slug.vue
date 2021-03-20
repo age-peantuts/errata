defede09b00f2accd242ce4f528f70fbdebec15c
orange634nty <orange634nty@gmail.com>
2021-03-20 18:25:51 +0900

    add errata page

---
+++ pages/errata/_slug.vue
@@ -0,0 +1,20 @@
+<template>
+  <article>
+    <h1>{{errata.title}}</h1>
+    <dl>
+      <dt>date</dt>
+      <dd>{{errata.date}}</dd>
+    </dl>
+
+    <nuxt-content :document="errata" />
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData ({ $content, params }) {
+    const errata = await $content('errata', params.slug || 'index').fetch()
+    return { errata }
+  }
+}
+</script>
---
+++ pages/errata/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div v-for="e in errata" :key="e.slug">
+      <nuxt-link :to="'/errata/'+ e.slug">{{e.title}} {{e.date}}</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  async asyncData ({ $content, params }) {
+    const query = await $content('errata' || 'index').limit(15)
+    const errata = await query.fetch()
+    return { errata }
+  }
+}
+</script>
